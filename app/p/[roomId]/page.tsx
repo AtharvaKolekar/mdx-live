cf1f8410294bae1dd789b6352b536910c67d1902
Atharva Kolekar <atharva.v.kolekar@gmail.com>
2025-09-06 01:56:04 +0530

    Copy works

--- app/p/[roomId]/page.tsx
+++ app/p/[roomId]/page.tsx
@@ -148,15 +148,121 @@ export default function RoomPage() {
     debouncedContentBroadcast(newContent);
     debouncedSaveContent(newContent);
   };
+  function escapeControlCharacters(str: string) {
+  return str
+    .replace(/\n/g, '\\n')  // Replace all newline characters with '\n'
+    .replace(/\r/g, '\\r')  // Replace all carriage return characters with '\r'
+    .replace(/\t/g, '\\t')  // Replace all tab characters with '\t'
+    .replace(/\f/g, '\\f')  // Replace all form feed characters with '\f')
+    .replace(/\v/g, '\\v');  // Replace all vertical tab characters with '\v')
+}
 
   const handleCopyContent = async () => {
     try {
-      await navigator.clipboard.writeText(content);
+      // Clean the content by removing backslashes, fixing newlines, and decoding HTML entities
+      let cleanedContent = content
+        .replace(/\\/g, '')     // Remove all backslashes
+        .replace(/\n\n/g, '\n') // Replace double newlines with single newlines
+        // Decode HTML entities
+        .replace(/&#x20;/g, ' ')   // Space
+        .replace(/&#x21;/g, '!')   // Exclamation mark
+        .replace(/&#x22;/g, '"')   // Quotation mark
+        .replace(/&#x23;/g, '#')   // Hash
+        .replace(/&#x24;/g, '$')   // Dollar sign
+        .replace(/&#x25;/g, '%')   // Percent
+        .replace(/&#x26;/g, '&')   // Ampersand
+        .replace(/&#x27;/g, "'")   // Apostrophe
+        .replace(/&#x28;/g, '(')   // Left parenthesis
+        .replace(/&#x29;/g, ')')   // Right parenthesis
+        .replace(/&#x2A;/g, '*')   // Asterisk
+        .replace(/&#x2B;/g, '+')   // Plus sign
+        .replace(/&#x2C;/g, ',')   // Comma
+        .replace(/&#x2D;/g, '-')   // Hyphen-minus
+        .replace(/&#x2E;/g, '.')   // Full stop
+        .replace(/&#x2F;/g, '/')   // Solidus
+        .replace(/&#x3A;/g, ':')   // Colon
+        .replace(/&#x3B;/g, ';')   // Semicolon
+        .replace(/&#x3C;/g, '<')   // Less-than sign
+        .replace(/&#x3D;/g, '=')   // Equals sign
+        .replace(/&#x3E;/g, '>')   // Greater-than sign
+        .replace(/&#x3F;/g, '?')   // Question mark
+        .replace(/&#x40;/g, '@')   // Commercial at
+        .replace(/&#x5B;/g, '[')   // Left square bracket
+        .replace(/&#x5C;/g, '\\')  // Reverse solidus
+        .replace(/&#x5D;/g, ']')   // Right square bracket
+        .replace(/&#x5E;/g, '^')   // Circumflex accent
+        .replace(/&#x5F;/g, '_')   // Low line
+        .replace(/&#x60;/g, '`')   // Grave accent
+        .replace(/&#x7B;/g, '{')   // Left curly bracket
+        .replace(/&#x7C;/g, '|')   // Vertical line
+        .replace(/&#x7D;/g, '}')   // Right curly bracket
+        .replace(/&#x7E;/g, '~')   // Tilde
+        // Common named entities
+        .replace(/&amp;/g, '&')
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>')
+        .replace(/&quot;/g, '"')
+        .replace(/&apos;/g, "'")
+        .replace(/&nbsp;/g, ' ')
+        // Generic decoder for any remaining numeric entities
+        .replace(/&#(\d+);/g, (match, dec) => String.fromCharCode(parseInt(dec, 10)))
+        .replace(/&#x([0-9A-Fa-f]+);/g, (match, hex) => String.fromCharCode(parseInt(hex, 16)));
+      
+      console.log(cleanedContent);
+     
+      await navigator.clipboard.writeText(cleanedContent);
       toast.success('Editor content copied to clipboard!');
     } catch (error) {
       // Fallback for browsers that don't support clipboard API
       const textArea = document.createElement('textarea');
-      textArea.value = content;
+      // Apply the same cleaning to fallback content
+      let cleanedContent = content
+        .replace(/\\/g, '')     // Remove all backslashes
+        .replace(/\n\n/g, '\n') // Replace double newlines with single newlines
+        // Decode HTML entities (same as above)
+        .replace(/&#x20;/g, ' ')
+        .replace(/&#x21;/g, '!')
+        .replace(/&#x22;/g, '"')
+        .replace(/&#x23;/g, '#')
+        .replace(/&#x24;/g, '$')
+        .replace(/&#x25;/g, '%')
+        .replace(/&#x26;/g, '&')
+        .replace(/&#x27;/g, "'")
+        .replace(/&#x28;/g, '(')
+        .replace(/&#x29;/g, ')')
+        .replace(/&#x2A;/g, '*')
+        .replace(/&#x2B;/g, '+')
+        .replace(/&#x2C;/g, ',')
+        .replace(/&#x2D;/g, '-')
+        .replace(/&#x2E;/g, '.')
+        .replace(/&#x2F;/g, '/')
+        .replace(/&#x3A;/g, ':')
+        .replace(/&#x3B;/g, ';')
+        .replace(/&#x3C;/g, '<')
+        .replace(/&#x3D;/g, '=')
+        .replace(/&#x3E;/g, '>')
+        .replace(/&#x3F;/g, '?')
+        .replace(/&#x40;/g, '@')
+        .replace(/&#x5B;/g, '[')
+        .replace(/&#x5C;/g, '\\')
+        .replace(/&#x5D;/g, ']')
+        .replace(/&#x5E;/g, '^')
+        .replace(/&#x5F;/g, '_')
+        .replace(/&#x60;/g, '`')
+        .replace(/&#x7B;/g, '{')
+        .replace(/&#x7C;/g, '|')
+        .replace(/&#x7D;/g, '}')
+        .replace(/&#x7E;/g, '~')
+        .replace(/&amp;/g, '&')
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>')
+        .replace(/&quot;/g, '"')
+        .replace(/&apos;/g, "'")
+        .replace(/&nbsp;/g, ' ')
+        .replace(/&#(\d+);/g, (match, dec) => String.fromCharCode(parseInt(dec, 10)))
+        .replace(/&#x([0-9A-Fa-f]+);/g, (match, hex) => String.fromCharCode(parseInt(hex, 16)));
+      
+      textArea.value = cleanedContent;
       document.body.appendChild(textArea);
       textArea.focus();
       textArea.select();
